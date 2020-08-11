57aeeb30307eecfc47ae67d99d8cd5e680a7e1ad
Tim Farahani <tfa@jcbachmann.de>
2020-08-11 11:16:30 +0200

    add termcolor submodule

--- cmake/Hunter/config.cmake
+++
@@ -1,3 +0,0 @@
-# Fix use of GoogleTest with GCC 9.1.0 by disabling GoogleMock
-# https://github.com/hunter-packages/googletest/issues/27
-hunter_config(GTest VERSION 1.8.0-hunter-p11 CMAKE_ARGS BUILD_GTEST=ON BUILD_GMOCK=OFF)
